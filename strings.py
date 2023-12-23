74fa9d64d973266819a42dfb29bc37048ae08246
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2023-12-23 19:18:41 -0300

    add: strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+# metodos para aprender
+
+text = 'Hello World in python string'
+
+# cuenta ocurrencias
+print(text.count('a'))
+print(text.count('o'))
+
+# cambia mayusculas y minusculas
+print(text.swapcase())
+
+# capilarizar primera letra del string
+print(text.capitalize())
+# inicio de cada palabra del texto en mayuscula
+print(text.title())
