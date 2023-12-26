e92782638a71b10add36d48a6e1529dd41c9b65b
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2023-12-25 22:12:04 -0300

    add: dicts in python

---
+++ dicts.py
@@ -0,0 +1,18 @@
+
+
+person = {
+    'name': 'Guillermo',
+    'last_name': 'Royo',
+    'langs': ['Python','Javascript','C#'],
+    'age' : 23,
+    'dev-level': 'Senior'
+}
+print(person)
+
+
+# eliminar elemento de la lista
+del person['last_name']
+print(person)
+
+
+print((8 / 2) + 4 * 8)
