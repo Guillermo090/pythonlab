b567a2839663fb991a43373d289f57f2c170e8d2
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2023-12-27 00:21:42 -0300

    add: set.py

---
+++ set.py
@@ -0,0 +1,32 @@
+# se pueden modificar
+# no tienen orden
+# no permiten duplicados
+
+
+# misma forma que un diccionario pero sin clave valor, duplicados se eliminan
+set_countries = { 'col', 'mex', 'bol', 'col' }
+print(set_countries)
+print(type(set_countries))
+
+set_numbers = { 1 , 2 ,2 , 3 ,11, 546}
+print(set_numbers)
+
+set_types = { 1 , 'hola', False, 12.32}
+print(set_types)
+
+set_from_string = set('hoolaa')
+print(set_from_string)
+
+set_from_tuples = set(('abc', 'cbv', 'as', 'abc'))
+print(set_from_tuples)
+
+set_from_list= set([1, 2, 454, 234, 1, 2])
+print(set_from_list)
+
+
+# crud sets
+set_countries = { 'col', 'mex', 'bol', 'col' }
+
+# add
+
+
