d786371f559f3917c17a4058f83821dfdcc39903
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2023-12-27 21:43:24 -0300

    dict comprehension with conditions

--- dicts.py
+++ dicts.py
@@ -16,3 +16,25 @@ print(person)
 
 
 print((8 / 2) + 4 * 8)
+
+
+
+# DICT COMPREHENSION
+import random
+countries = ['col','mex','bol','pe']
+
+population_v2 = { 
+                    country: random.randint(1,100)
+                    for country in countries
+                }
+print(population_v2)
+
+# se puede dividir un list comprehension en llave valor cuando se les da un dict iterado de entrada
+result = {country:population for (country,population) in population_v2.items() if population > 20}
+print(result)
+
+
+text = 'Hola, soy Nicolas'
+
+unique = { c:c.upper() for c in text if c in 'aeiou' }
+print(unique)
