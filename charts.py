8e5bc3305a00f28c47bbf11f37f182b228e60069
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-01-16 23:36:55 -0300

    add: new pie chart with image creation

--- charts.py
+++ charts.py
@@ -10,9 +10,19 @@ def generate_pie_chart(labels, values):
     ax.pie(values, labels=labels)
     ax.axis('equal')
     plt.show()
+    
+def generate_second_pie_chart():
+    labels = ['A', 'B', 'C']
+    values = [200, 34, 120]
+    
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig('.pie.png')
+    plt.close()
 
 if __name__ == '__main__':
     labels = ['a','b','c']
     values = [10, 40, 80]
     # generate_bar_chart(labels, values)
-    generate_pie_chart(labels, values)
+    # generate_pie_chart(labels, values)
+    generate_second_pie_chart()
